6533cc1d9ee5c20d2bb0b587014caaaa0ade0c53
oikwsat <oikwsat@gmail.com>
2019-11-26 22:30:56 +0900

    1.2.2 チャットルームのモデル化

--- chapter01/chat/room.go
+++ chapter01/chat/room.go
@@ -3,4 +3,10 @@ package main
 type room struct {
 	// forwardは他のクライアントに転送するためのメッセージを保持するチャネルです
 	forward chan []byte
+	// joinはチャットルームに参加しようとしているクライアントのためのチャネルです
+	join chan *client
+	// leaveはチャットルームから退室しようとしているクライアントのためのチャネルです
+	leave chan *client
+	// clientsには在室しているすべてのクライアントが保持されます
+	clients map[*client]bool
 }
