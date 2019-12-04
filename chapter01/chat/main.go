87155a29344e3dd542a9f44922cfe0cdba0be482
oikwsat <oikwsat@gmail.com>
2019-12-04 23:32:42 +0900

    1.4.2 traceパッケージの利用方法

--- chapter01/chat/main.go
+++ chapter01/chat/main.go
@@ -5,8 +5,11 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"os"
 	"path/filepath"
 	"sync"
+
+	"github.com/matryer/goblueprints/chapter1/trace"
 )
 
 type templateHandler struct {
@@ -29,6 +32,7 @@ func main() {
 	var addr = flag.String("addr", ":8080", "アプリケーションのアドレス")
 	flag.Parse() // フラグを解釈します
 	r := newRoom()
+	r.tracer = trace.New(os.Stdout)
 	http.Handle("/", &templateHandler{filename: "chat.html"})
 	http.Handle("/room", r)
 
