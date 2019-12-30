43f66d95faa0f8d48acaa7487d71004a5a421ad4
oikwsat <oikwsat@gmail.com>
2019-12-30 23:11:20 +0900

    3.1.2 アバターのURLの送信

--- chapter01/chat/client.go
+++ chapter01/chat/client.go
@@ -24,6 +24,9 @@ func (c *client) read() {
 		if err := c.socket.ReadJSON(&msg); err == nil {
 			msg.When = time.Now()
 			msg.Name = c.userData["name"].(string)
+			if AvatarURL, ok := c.userData["avatar_url"]; ok {
+				msg.AvatarURL = AvatarURL.(string)
+			}
 			c.room.forward <- msg
 		} else {
 			break
