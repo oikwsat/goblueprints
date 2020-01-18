0f7aacba2690b6b39801e3bf2dac39c3500f414f
oikwsat <oikwsat@gmail.com>
2020-01-19 00:44:02 +0900

    3.3.3 アップロードされたファイルの処理

--- chapter01/chat/main.go
+++ chapter01/chat/main.go
@@ -54,6 +54,7 @@ func main() {
 	http.HandleFunc("/auth/", loginHandler)
 	http.Handle("/room", r)
 	http.Handle("/upload", &templateHandler{filename: "upload.html"})
+	http.HandleFunc("/uploader", uploaderHandler)
 	http.HandleFunc("/logout/", func(w http.ResponseWriter, r *http.Request) {
 		http.SetCookie(w, &http.Cookie{
 			Name:   "auth",
